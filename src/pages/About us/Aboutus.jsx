eef5372bac910d204b42018aaf0b4144f092dbe0
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2023-05-14 16:36:21 +0200

    About Us page complete

--- src/pages/About us/Aboutus.jsx
+++ src/pages/About us/Aboutus.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Button } from '@mui/material';
 
 const AboutUs = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ maxWidth: '800px' }}>
-        <Typography variant="h4" component="h1">
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', paddingTop: '50px' }}>
+      <div style={{ maxWidth: '800px', padding: '0 16px' }}>
+        <Typography variant="h4" component="h1" style={{ marginBottom: '32px' }}>
           About Us
         </Typography>
-        <Typography variant="body1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lectus auctor, efficitur risus vitae, eleifend lectus. Proin venenatis, risus eu efficitur dictum, elit felis feugiat lacus, sed eleifend eros odio eget libero. In convallis arcu quis efficitur efficitur. Maecenas commodo diam eu nisi consequat, vel egestas odio lobortis. Mauris dapibus sapien ac erat elementum, ut auctor ipsum efficitur. Etiam non ultricies nulla, non bibendum velit. Nunc ornare lorem eu arcu malesuada suscipit. Sed cursus auctor arcu a pharetra. Duis maximus nunc sit amet bibendum finibus. Nulla facilisi. Donec aliquam malesuada risus. Vestibulum volutpat est vel euismod facilisis. Integer eget tellus in felis tincidunt hendrerit.
+        <Typography variant="body1" style={{ textAlign: 'justify', marginTop: '16px', marginBottom: '32px' }}>
+          At Lala Le Chat, we are passionate about the well-being of our feline friends and understand how important it is to ensure that they always have access to fresh food and water. That is why our team tests all of our products to ensure that users benefit from the highest range on the market. The products we choose are of high quality and have been selected with the aim of making cats happier. All of our products have been tested by Lala before being sold.
         </Typography>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button variant="contained" style={{ backgroundColor: 'black', color: 'white', textTransform:'none' }}>
+            More on Lala
+          </Button>
+        </div>
       </div>
     </div>
   );
