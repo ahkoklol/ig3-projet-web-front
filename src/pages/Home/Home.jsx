52fb641ffbc5723175b8daa2fde0332ee50400b1
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2023-05-16 08:45:34 +0200

    adjust home page first image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,51 +6,51 @@ import siameseCat from '../../assets/siamese-cat-laying-in-white-bedding.jpg';
 
 const Home = () => {
   return (
-    <div style={{
+    <Container
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
       backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${catEnjoying}), url(${siameseCat})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
-      height: '100vh',
+      height: '79vh',
       position: 'relative',
-      display: 'flex',
-      alignItems: 'center',
-      
-    }}>
-      <Container sx={{
-        mt: 18,
-        position: 'relative',
-        zIndex: 1,
-        
-      }}>
-        <style>
-          {`
-            .white-text {
-              color: white;
-            }
-          `}
-        </style>
-        <Typography variant="h3" align="center" gutterBottom className="white-text">
-          Welcome to Our Online Store!
-        </Typography>
-        <Typography variant="body1" align="center" gutterBottom className="white-text">
-          Here you will find a wide selection of high-quality products at affordable prices.
-        </Typography>
-        <Typography variant="body1" align="center" gutterBottom className="white-text">
-          Take a look at our catalog to see all the products we have available.
-        </Typography>
-        <Grid container spacing={2} justifyContent="center" sx={{ mt: 4 }}>
-          <Grid item>
-            <Button variant="contained" sx={{ backgroundColor: 'black', color: 'white', textTransform: 'none' }} href="/catalog">
-              View Catalog
-            </Button>
-          </Grid>
+      maxWidth: '100%',
+      mt: 19.5,
+      zIndex: 1,
+    }}
+    >
+      <style>
+        {`
+          .white-text {
+            color: white;
+          }
+        `}
+      </style>
+      <Typography variant="h3" align="center" gutterBottom className="white-text">
+        Welcome to Our Online Store!
+      </Typography>
+      <Typography variant="body1" align="center" gutterBottom className="white-text">
+        Here you will find a wide selection of high-quality products at affordable prices.
+      </Typography>
+      <Typography variant="body1" align="center" gutterBottom className="white-text">
+        Take a look at our catalog to see all the products we have available.
+      </Typography>
+      <Grid container spacing={2} justifyContent="center" sx={{ mt: 4 }}>
+        <Grid item>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: 'black', color: 'white', textTransform: 'none' }}
+            href="/catalog"
+          >
+            View Catalog
+          </Button>
         </Grid>
-      </Container>
-    </div>
-    
-      
-
+      </Grid>
+    </Container>
   );
 };
 
