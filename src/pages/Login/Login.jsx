11c082491cc6fc795754ba3745242d204c6315ed
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2023-05-29 20:54:34 +0200

    login back to before

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -21,12 +21,12 @@ function SignIn() {
   const { updateSessionID } = useContext(SessionContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [orderDetailsID, setOrderDetailsID] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Add this line to define the isLoggedIn state
 
   useEffect(() => {
+    // Check if the user is already logged in on component mount
     checkLoginStatus();
-  }, []);
+  }, []); // Empty dependency array to run the effect only once
 
   const checkLoginStatus = () => {
     const token = localStorage.getItem('token');
@@ -35,7 +35,6 @@ function SignIn() {
       const userID = decodedToken.idUser;
       updateSessionID(decodedToken.sessionID);
       setIsLoggedIn(true);
-      createOrderDetails(userID); // Call the function to create order details
     }
   };
 
@@ -59,11 +58,11 @@ function SignIn() {
             const token = response.data.token;
             const decodedToken = jwt_decode(token);
             const userID = decodedToken.idUser;
-            updateSessionID(response.data.sessionID);
-            localStorage.setItem('token', token);
+            updateSessionID(response.data.sessionID); // Update the session ID using the context function
+            localStorage.setItem('token', token); // Store the token in local storage
             toast.success('Welcome back ' + email + ' !');
-            createShoppingSession(userID);
-            setIsLoggedIn(true);
+            createShoppingSession(userID); // Call the function to create the shopping session
+            setIsLoggedIn(true); // Set isLoggedIn to true on successful login
           } else {
             console.log('Login failed');
             toast.error('Something went wrong when logging in');
@@ -76,10 +75,13 @@ function SignIn() {
 
   const createShoppingSession = async (userID) => {
     try {
-      const response = await axios.post(`${process.env.REACT_APP_API_URL}/shoppingSession`, {
-        userID: userID,
-        total: 0,
-      });
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/shoppingSession`,
+        {
+          userID: userID,
+          total: 0, // Replace with the actual total value if applicable
+        }
+      );
 
       if (response.status === 201) {
         console.log('Shopping session created successfully');
@@ -97,14 +99,16 @@ function SignIn() {
 
   const createOrderDetails = async (userID) => {
     try {
-      const response = await axios.post(`${process.env.REACT_APP_API_URL}/orderDetails`, {
-        userID: userID,
-        price: 0,
-      });
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/orderDetails`,
+        {
+          userID: userID,
+          price: 0, // Replace with the actual price value if applicable
+        }
+      );
 
       if (response.status === 201) {
         console.log('Order details created successfully');
-        setOrderDetailsID(response.data.OrderDetailsID);
         // Handle the successful creation of order details
       } else {
         console.log('Error creating order details');
@@ -116,69 +120,116 @@ function SignIn() {
     }
   };
 
+  const handleLogout = () => {
+    // Clear the session data and logged-in state
+    localStorage.removeItem('token');
+    updateSessionID('');
+    setIsLoggedIn(false);
+    toast.success('Logged out successfully');
+  };
+
   const theme = createTheme();
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component="main" maxWidth="xs" sx={{ marginBottom: '50px' }}>
         <CssBaseline />
         <Box
           sx={{
-            marginTop: '200px',
+            marginTop: 22,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            marginBottom: '50px',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} style={{ backgroundColor: 'black', color: 'white', textTransform:'none' }}>
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="" variant="body2" color="error">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2" color="error">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+          {isLoggedIn ? (
+            <>
+              <Typography component="h1" variant="h5">
+                Welcome back!
+              </Typography>
+              <Button
+                onClick={handleLogout}
+                variant="contained"
+                color="secondary"
+                sx={{ mt: 2 }}
+              >
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 2, mb: 2 }}
+                  style={{ backgroundColor: 'black', color: 'white', textTransform: 'none' }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link
+                      href="#"
+                      variant="body2"
+                      style={{ color: '#d3003f', textDecorationColor: '#d3003f' }}
+                    >
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link
+                      href="/register"
+                      variant="body2"
+                      style={{ color: '#d3003f', textDecorationColor: '#d3003f' }}
+                    >
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </>
+          )}
         </Box>
+        <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 8, mb: 4 }}>
+          {'Copyright © '}
+          <Link color="inherit" href="https://mui.com/">
+            Lala le chat
+          </Link>{' '}
+          {new Date().getFullYear()}
+          {'.'}
+        </Typography>
       </Container>
     </ThemeProvider>
   );
